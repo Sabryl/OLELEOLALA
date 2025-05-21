4ba0f288c486f47cc30c0a94de8959e23b0fe61e
Sabryl <iiloumeika@gmail.com>
2025-05-21 20:15:41 +0800

    Delete CITS2005_2025S1_Project(1) (1)/src directory

--- CITS2005_2025S1_Project(1) (1)/src/ai/AI.java
+++
@@ -1,8 +0,0 @@
-package ai;
-
-import game.*;
-
-public interface AI {
-    // Estimates the best move for the current player
-    Move getCurrentPlayerMove(Game game);
-}
--- CITS2005_2025S1_Project(1) (1)/src/game/PieceColour.java
+++
@@ -1,5 +0,0 @@
-package game;
-
-public enum PieceColour {
-    NONE, WHITE, BLACK
-}
